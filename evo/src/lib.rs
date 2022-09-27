e98fd6c7173614513332261398287e4e1cf70004
Kedap <kedap.dev@protonmail.com>
2022-09-26 20:24:54 -0500

    feat: API design

--- evo/src/lib.rs
+++ evo/src/lib.rs
@@ -1,8 +1,22 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+//! # Evo
+//! Evo is a graphical environment variable editor for Linux (librery).
+
+/// Fetch current environment variables
+pub fn fetch_vars() {
+    todo!();
+}
+
+/// Set a new(s) variable(s)
+pub fn set_var() {
+    todo!();
+}
+
+/// Edit an existing variable
+pub fn edit_var() {
+    todo!();
+}
+
+/// Unset an existing variable
+pub fn unset_var() {
+    todo!();
 }
