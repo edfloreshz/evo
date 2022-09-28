8d968642824da6dc7848d23a1068c20e84165b80
Eduardo Flores <edfloreshz@gmail.com>
2022-09-27 18:47:35 -0600

    Initial implementation of fetch_vars

--- evo/src/lib.rs
+++ evo/src/lib.rs
@@ -1,22 +1,39 @@
 //! # Evo
 //! Evo is a graphical environment variable editor for Linux (library).
 
+use std::collections::HashMap;
+
 /// Fetch current environment variables
-pub fn fetch_vars() {
-    todo!();
+pub fn fetch_vars() -> HashMap<String, String> {
+    let mut vars = HashMap::new();
+    for (key, value) in std::env::vars_os() {
+        vars.insert(
+            key.to_str().unwrap().to_string(),
+            value.to_str().unwrap().to_string(),
+        );
+    }
+    vars
 }
 
 /// Set a new(s) variable(s)
-pub fn set_var() {
+pub fn set_var(_key: &str, _value: String) {
     todo!();
 }
 
 /// Edit an existing variable
-pub fn edit_var() {
+pub fn edit_var(_key: &str) {
     todo!();
 }
 
 /// Unset an existing variable
-pub fn unset_var() {
+pub fn unset_var(_key: &str) {
     todo!();
+}
+
+mod tests {
+    #[test]
+    fn test_fetch_vars() {
+        let vars = crate::fetch_vars();
+        assert_eq!(vars["SHLVL"], "0");
+    }
 }
